0a6c1441e1f6c7f8865894499f3400f966231992
sainik-create <68194201+sainik-create@users.noreply.github.com>
2024-01-05 07:08:09 +0530

    Add rainbow effect on hovering the divs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let grid_size = prompt("Please enter the number of rows");
+let grid_size = prompt("Please enter the the number of squares per side");
 
 if (Number.isFinite(parseInt(grid_size)) && parseInt(grid_size) > 0 && parseInt(grid_size) <= 100) {
   const container = document.querySelector('#container');
@@ -13,8 +13,24 @@ if (Number.isFinite(parseInt(grid_size)) && parseInt(grid_size) > 0 && parseInt(
       const tar_div = document.createElement('div');
       tar_div.classList.add('terDivs');
       sec_div.appendChild(tar_div);
+
+
+      tar_div.addEventListener("mouseenter", hoverRainbow);
     }
   }
 } else {
-    alert("Please enter a positive integar less than or equal to 100");
+    alert("Please enter an integar greater than 0 and less than 101")
+}
+
+function RGBcolor() {
+  var R = Math.floor(Math.random() * 256);
+  var G = Math.floor(Math.random() * 256);
+  var B = Math.floor(Math.random() * 256);
+  var randomcolor = "rgb(" + R + "," + G + "," + B + ")";
+  return randomcolor;
+}
+
+function hoverRainbow() {
+  const color = RGBcolor();
+  this.style.backgroundColor = color;
 }
